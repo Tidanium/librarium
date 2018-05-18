f1f252e6430876e82c01f685e8586dff3531ade6
Tidanium <ryan@ryanphillips.org>
2018-05-17 20:28:22 -0700

    Update log.py

--- server/modules/utils/log.py
+++ server/modules/utils/log.py
@@ -1,4 +1,13 @@
 import os
 import server
 
-pathlist = server.baseDir+'temp'
+path = server.baseDir+'temp'
+if not os.path.isdir(path):
+  os.mkdir(path)
+fileList = [path+f'log-server', path+'argon2',]
+def checkLogExists(file:str=None):
+  if file:
+    fileList.append(path+'/'+file)
+  for f in fileList:
+    if os.path.isfile(f):
+      pass
