f587400d524b676ea45ab5ceb1233764c3867fb0
Tidanium <ryan@ryanphillips.org>
2018-05-17 20:28:12 -0700

    Update security.py

--- server/modules/utils/security.py
+++ server/modules/utils/security.py
@@ -1,4 +1,9 @@
-import secrets
+# general pypi packages
+import os, platform, secrets, uuid
+
+# server-specific packages
+from . import settings
+from .. import file_management
 
 try:
   import argon2
@@ -6,6 +11,7 @@ except ImportError:
   print('Run "pip install argon2_cffi" to continue.')
 
 class type(object):
+  """Types used for SecurityBase.generateSecret(type:int)"""
   def __init__(self):
     self.user=type._user()
     self.session=type._session()
@@ -25,6 +31,27 @@ class type(object):
   
 class account(object):
   
-  async def generate(self, name, email, gpg_pubkey, *args, **kwargs):
+  async def generate(self, name:str, email:str, gpg_pubkey, *args, **kwargs):
+    pass
   
+class SecurityBase:
+
+  async def generateSecret(self, type:int):
+    """Types are in the type(object) class.
+      If you're changing this, leave the length at 32 so you don't break anything."""
+    if type == 0: # user
+      return secrets.token_bytes(32) # used as the salt for argon2id password + oauth2 encrypting
+    elif type == 1: # session
+      return secrets.token_hex(32) # used to encrypt the streams for ReST
+    elif type == 2: # connection
+      return secrets.token_urlsafe(32) # used to identify which connection is which while streaming (downloading/uploading)
+    else:
+      raise NotImplementedError # in case you fuckers change any code here.
+
+class GPG:
+  
+  @staticmethod
+  def checkGPGExists():
+    """Name self-explanatory."""
+    return file_management.executableIsExecutable('gpg')
   
